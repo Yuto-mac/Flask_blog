8610f80242221fce39443a8981a37512242886f6
yutowilliam <1762934741@qq.com>
2022-03-08 17:54:01 +0800

    first migration

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,14 @@
+import imp
 from flask import Flask
+from app.config import Config
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 
 app = Flask(__name__)
 app.debug = True
-from app import routes
+app.config.from_object(Config)
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+
+
+from app import routes,  models
